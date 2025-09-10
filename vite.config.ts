2b6bfa91cc87e560560cb10a9118ae98edef1ddc
EladAurumons <elad.goldenberg@gmail.com>
2025-09-10 17:47:36 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',             // ← important for a custom domain
+  base: '/',          // required for a custom domain at the root
   plugins: [react()],
 })
